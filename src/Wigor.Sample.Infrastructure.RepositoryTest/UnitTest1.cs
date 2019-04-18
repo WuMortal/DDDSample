582d95cc0cc530ca9015c2af3f5bf9b891e3e5b1
Wigor <1170621609@qq.com>
2019-04-18 22:39:04 +0800

    [add] 添加仓储单元测试

---
+++ src/Wigor.Sample.Infrastructure.RepositoryTest/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Wigor.Sample.Infrastructure.RepositoryTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
