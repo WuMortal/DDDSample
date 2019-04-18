4a52f0adc3448f799748f2a909853242bb401ce2
Wigor <1170621609@qq.com>
2019-04-18 22:53:25 +0800

    [add] 完成单元测试

---
+++ src/Wigor.Sample.Infrastructure.RepositoryTest/DBInit.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Wigor.Sample.Infrastructure.RepositoryTest
+{
+    public class DBInit
+    {
+        public static IFreeSql DBFreeSql
+        {
+            get
+            {
+                var fsql = new FreeSql.FreeSqlBuilder()
+                            .UseConnectionString(FreeSql.DataType.Sqlite, @"Data Source=|DataDirectory|\document.db;Pooling=true;Max Pool Size=10")
+                             .UseAutoSyncStructure(true) //自动迁移实体的结构到数据库
+                            .Build();
+                return fsql;
+            }
+        }
+    }
+}
---
+++ src/Wigor.Sample.Infrastructure.RepositoryTest/UserRepositoryUnitTest.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Wigor.Sample.Domain.Entity;
+using Wigor.Sample.Infrastructure.Repository;
+
+namespace Wigor.Sample.Infrastructure.RepositoryTest
+{
+    [TestClass]
+    public class UserRepositoryUnitTest
+    {
+
+        [TestMethod]
+        public void TestAddMethod_Success()
+        {
+            using (var userRepository = new UserRepository(DBInit.DBFreeSql))
+            {
+                var id = Guid.NewGuid();
+                userRepository.Insert(new UserEntity
+                {
+                    Id = id
+                });
+
+                var userEntity = userRepository.Get(id);
+
+                Assert.IsTrue(id == userEntity.Id);
+            }
+        }
+    }
+}
