92f6901a13db33c769cf8af45fd444d8b6af56eb
Wigor <1170621609@qq.com>
2019-04-19 20:53:53 +0800

    [add] 添加 IUserService 和 UserService

---
+++ src/Wigor.Sample.Application/Service/UserService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Wigor.Sample.ApplicationContract;
+
+namespace Wigor.Sample.Application.Service
+{
+    public class UserService : IUserService
+    {
+    }
+}
---
+++ src/Wigor.Sample.ApplicationContract/IUserService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Wigor.Sample.ApplicationContract
+{
+    public interface IUserService
+    {
+    }
+}
